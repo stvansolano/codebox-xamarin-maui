0a1dadff4cd08dd1856d83685442ff571041d442
Esteban Solano <stvansolano>
2021-12-27 23:50:37 -0600

    Clean-up

---
+++ MobileApp/MobileApp/App.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MobileApp
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			MainPage = new ContentPage { Content = new Label { Text = "Hello, World!", VerticalOptions = LayoutOptions.Center, HorizontalOptions = LayoutOptions.Center } };
+		}
+	}
+}
