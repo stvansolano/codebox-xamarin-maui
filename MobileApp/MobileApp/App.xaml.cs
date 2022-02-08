dd6ee7ecee8a64f0b477ecc5bbdee0929c134215
stvansolano <stvansolano@outlook.com>
2022-02-08 17:41:43 -0600

    Default sample

--- MobileApp/MobileApp/App.xaml.cs
+++ MobileApp/MobileApp/App.xaml.cs
@@ -9,11 +9,18 @@ namespace MobileApp
 		{
 			InitializeComponent();
 
+			MainPage = new ContentPage { Content = new Label { Text = "Hello, World!", VerticalOptions = LayoutOptions.Center, HorizontalOptions = LayoutOptions.Center } };
+		}
+
+		private Page UseTabs()
+		{
 			TabsPage tabs;
 			MainPage = tabs = new TabsPage();
 
 			tabs.Children.Add(new FontsView() { Title = "Font Families" });
-			tabs.Children.Add(new FontSample("Lobster") { Title = "Sample 2" });
+			tabs.Children.Add(new FontSample("Lobster") { Title = "Font sample" });
+
+			return tabs;
 		}
 	}
 }
