25d864096282353ed732b65e1cbc2b815bf68551
stvansolano <stvansolano@outlook.com>
2022-02-08 17:39:07 -0600

    2) Register font

--- MobileApp/MobileApp/AssemblyInfo.cs
+++ MobileApp/MobileApp/AssemblyInfo.cs
@@ -5,3 +5,4 @@ using Xamarin.Forms.Xaml;
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: Preserve(AllMembers = true)]
 [assembly: ExportFont("Lobster-Regular.ttf", Alias = "Lobster")]
+[assembly: ExportFont("ArchitectsDaughter-Regular.ttf", Alias = "ArchitectsDaughter")]
